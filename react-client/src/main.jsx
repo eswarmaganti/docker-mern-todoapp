0b96bef787639ce547d154c0e56f6d5e47ba3b74
Eswar Krishna Maganti <maganti.ek@gmail.com>
2023-10-02 15:56:40 +0530

    react-client image is updated with nginx

--- react-client/src/main.jsx
+++ react-client/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@mui/material";
-import theme from "./theme.ts";
+import theme from "./theme.js";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store } from "./store/index.js";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
